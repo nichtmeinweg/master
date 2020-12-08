9f4fd60901777717c606024d321107b6001995a8
Your name <anymail@anycompany.com>
2020-12-08 13:26:22 +0100

    init

---
+++ data/data.js
@@ -0,0 +1,14 @@
+const EVENTS = [
+    {
+        "description": "&Uuml;ber uns",
+        "text": "Wir kritisieren die Gleichg&uuml;ltigkeit gegen&uuml;ber den an COVID-19 erkrankten und verstorben in der Schweiz. Es gibt keine G&uuml;terabw&auml;gung zwischen Gesundheit, Wirtschaft und Gesellschaft. #LongCovid #NichtMeinWeg",
+        "date": "07.12.2020",
+        "image": "img/img-01.jpg"
+    },
+    {
+        "description": "&Uuml;ber uns",
+        "text": "Wir kritisieren die Gleichg&uuml;ltigkeit gegen&uuml;ber den an COVID-19 erkrankten und verstorben in der Schweiz. Es gibt keine G&uuml;terabw&auml;gung zwischen Gesundheit, Wirtschaft und Gesellschaft. #LongCovid #NichtMeinWeg",
+        "date": "07.12.2020",
+        "image": "img/img-01.jpg"
+    },
+]
